24b5ccfa74ec27b937b08759bc5b8dfaae945b9c
aryika jain <aryikajain1@gmail.com>
2025-11-19 23:42:45 +0530

    build for deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/aryikajain.github.io/", // 👈 YOUR GITHUB REPO NAME
+  base: "/PORTFOLIO/", // 🚀 your repo name here EXACTLY
 });
